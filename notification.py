ecef8aabfc39f2f2fc5ba61758a751eda151c899
Chidananda Sahoo <56102668+cnsnaya@users.noreply.github.com>
2020-12-21 13:21:49 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 
 top = Tk()
 
+top.withdraw()     #Removes the tkinter's extra window
 top.geometry("100x100")
 
 messagebox.showinfo("Alert!!","Your customer wants to say something")
